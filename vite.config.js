54e58dd0bb0c46197dc5ab67ca139e5184133d43
skyfilmaker <skyfilmaker@gmail.com>
2026-08-30 16:12:55 +0200

    Add application version definition to Vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "./",
+  define: {
+    __APP_VERSION__: JSON.stringify(process.env.VITE_APP_VERSION || "dev"),
+  },
 });
